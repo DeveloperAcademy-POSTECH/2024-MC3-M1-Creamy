2a1b02d4c2448ba7c61b5bb0403b90d17d30ee18
Park Junwoo <ays99111@naver.com>
2024-07-26 17:17:41 +0900

    [Bug/#10] new project file foldering

---
+++ TurtleNeck/TurtleNeck/Presentation/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  TurtleNeck
+//
+//  Created by 박준우 on 7/26/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ TurtleNeck/TurtleNeck/Presentation/Onboarding/OnboardingView.swift
@@ -0,0 +1,18 @@
+//
+//  OnboardingView.swift
+//  TurtleNeck
+//
+//  Created by 박준우 on 7/26/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}

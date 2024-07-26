59df60b2389a19bbf6415d3a824fc41d57e9f1c3
HyunJaeyeon <bomul0109@gmail.com>
2024-07-26 15:01:11 +0900

    [Chore/#7] AlertStatistic 모델 추가
    - 날짜(date), 측정시간(time), 알림받은 횟수(date) 추가

---
+++ TurtleNeck/TurtleNeck/Data/Model/AlertStatistic.swift
@@ -0,0 +1,22 @@
+//
+//  AlertStatistic.swift
+//  TurtleNeck
+//
+//  Created by Hyun Jaeyeon on 7/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class AlertStatistic {
+    var date: Date
+    var time: Int
+    var count: Int
+    
+    init(date: Date, time: Int, count: Int) {
+        self.date = date
+        self.time = time
+        self.count = count
+    }
+}

c8f002722864563b508dc9ec89d1abb9e4f33636
HyunJaeyeon <bomul0109@gmail.com>
2024-07-30 01:27:20 +0900

    [Chore/#13] NotiPermissionView UI 반영

--- TurtleNeck/TurtleNeck/Presentation/Onboarding/NotiPermissionView.swift
+++ TurtleNeck/TurtleNeck/Presentation/Onboarding/NotiPermissionView.swift
@@ -11,19 +11,42 @@ struct NotiPermissionView: View {
     @EnvironmentObject var router: Router
     
     var body: some View {
-        VStack{
-            Text("알림 허용이 필요해요")
-            Text("더 올바른 자세 유지를 도울 수 있도록\n 알림 허용이 필요해요")
+        VStack(spacing: 0){
+            VStack(spacing: 0){
+                Image("notificationImg")
+                    .padding(.bottom, 28)
+                
+                Text("알림 허용이 필요해요")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .padding(.bottom, 16)
+                
+                Text("더 올바른 자세 유지를 도울 수 있도록\n알림 허용이 필요해요")
+                    .multilineTextAlignment(.center)
+                    .lineSpacing(4)
+                    .font(.callout)
+            }
+            
+            Spacer()
             
             Button {
+                print("clicked")
                 router.navigate(to: .checkDevice)
             } label: {
                 Text("다음")
+                    .foregroundStyle(.green)
+                    .frame(width: 200, height: 33)
             }
+            .buttonStyle(.plain)
+            .border(.black)
         }
+        .padding(.top, 132)
+        .padding(.bottom, 61)
     }
 }
 
 #Preview {
     NotiPermissionView()
+        .frame(width: 560, height: 560)
+        .background(.white)
 }

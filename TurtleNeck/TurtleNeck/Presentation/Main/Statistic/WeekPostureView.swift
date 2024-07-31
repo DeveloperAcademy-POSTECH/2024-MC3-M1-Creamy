76c69caa5302520a6d1d4e3e87a3e0eeda2e2219
Seo <kirin2211@naver.com>
2024-07-31 17:29:46 +0900

    [Feat#22]Create WeekPostureView

--- TurtleNeck/TurtleNeck/Presentation/Main/Statistic/WeekPostureView.swift
+++ TurtleNeck/TurtleNeck/Presentation/Main/Statistic/WeekPostureView.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct WeekPostureView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading) {
+            Text("지난 7일간")
+                .font(.pretendardBold10)
+                .foregroundColor(.black)
+                .padding(.top, 8)
+            Text("시간당 평균적으로 받은 알림 횟수")
+                .font(.pretendardBold10)
+                .foregroundColor(.black)
+            
+            HStack(alignment: .bottom){
+                ForEach(0..<7) { _ in
+                    VStack{
+                        Text("숫자")
+                            .font(.pretendardMedium10)
+                            .foregroundColor(Color.chart)
+                        
+                        Rectangle()
+                            .fill(Color.chart)
+                            .frame(width: 16, height: CGFloat(60))
+                            .padding(.horizontal, 4.5)
+                            .clipShape(
+                                .rect(
+                                    topLeadingRadius: 20,
+                                    bottomLeadingRadius: 0,
+                                    bottomTrailingRadius: 0,
+                                    topTrailingRadius: 20
+                                )
+                            )
+                        Text("07/31").font(.pretendardRegular10).foregroundColor(.black)
+                            .frame(width: 29)
+                    }
+                }
+            }
+            .frame(width: 251,height: 120)
+            .padding(.top, 16)
+        }
     }
 }
 
--- TurtleNeck/TurtleNeck/Resource/Extensions/FontExtension.swift
+++ TurtleNeck/TurtleNeck/Resource/Extensions/FontExtension.swift
@@ -11,20 +11,24 @@ import SwiftUI
 extension Font {
     
     // Bold
-    static let pretendardBold14: Font = .custom("Pretendard-Bold", size: 12)
+    static let pretendardBold12: Font = .custom("Pretendard-Bold", size: 12)
+    static let pretendardBold10: Font = .custom("Pretendard-Bold", size: 10)
     
     // SemiBold
-    static let pretendardSemiBold14: Font = .custom("Pretendard-SemiBold", size: 12)
+    static let pretendardSemiBold12: Font = .custom("Pretendard-SemiBold", size: 12)
     
     // Medium
-    static let pretendardMedium16: Font = .custom("Pretendard-Medium", size: 12)
+    static let pretendardMedium12: Font = .custom("Pretendard-Medium", size: 12)
+    static let pretendardMedium10: Font = .custom("Pretendard-Medium", size: 10)
+    static let pretendardMedium8: Font = .custom("Pretendard-Medium", size: 8)
     
     // Regular
     static let pretendardRegular14: Font = .custom("Pretendard-Regular", size: 14)
     static let pretendardRegular13: Font = .custom("Pretendard-Regular", size: 13)
     static let pretendardRegular12: Font = .custom("Pretendard-Regular", size: 12)
+    static let pretendardRegular10: Font = .custom("Pretendard-Regular", size: 10)
     
     // Thin
-    static let pretendardThin14: Font = .custom("Pretendard-Thin", size: 12)
+    static let pretendardThin12: Font = .custom("Pretendard-Thin", size: 12)
     
 }

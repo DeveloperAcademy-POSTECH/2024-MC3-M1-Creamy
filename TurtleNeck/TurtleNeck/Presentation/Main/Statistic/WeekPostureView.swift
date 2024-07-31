6f7bf10180838ef04df541113b67c8bb9751a03d
Seo <kirin2211@naver.com>
2024-07-31 17:53:34 +0900

    [Ref/#22]Refactoring WeekPostureView padding

--- TurtleNeck/TurtleNeck/Presentation/Main/Statistic/WeekPostureView.swift
+++ TurtleNeck/TurtleNeck/Presentation/Main/Statistic/WeekPostureView.swift
@@ -10,25 +10,25 @@ import SwiftUI
 struct WeekPostureView: View {
     var body: some View {
         
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 0) {
             Text("지난 7일간")
                 .font(.pretendardBold10)
                 .foregroundColor(.black)
-                .padding(.top, 8)
+                .padding(.top, 17)
             Text("시간당 평균적으로 받은 알림 횟수")
                 .font(.pretendardBold10)
                 .foregroundColor(.black)
             
             HStack(alignment: .bottom){
                 ForEach(0..<7) { _ in
-                    VStack{
+                    VStack(spacing: 2){
                         Text("숫자")
                             .font(.pretendardMedium10)
                             .foregroundColor(Color.chart)
                         
                         Rectangle()
                             .fill(Color.chart)
-                            .frame(width: 16, height: CGFloat(60))
+                            .frame(width: 16, height: CGFloat(88))
                             .padding(.horizontal, 4.5)
                             .clipShape(
                                 .rect(
@@ -44,7 +44,7 @@ struct WeekPostureView: View {
                 }
             }
             .frame(width: 251,height: 120)
-            .padding(.top, 16)
+            .padding(EdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0))
         }
     }
 }

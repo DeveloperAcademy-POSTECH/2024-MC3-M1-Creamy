1aa4957f4052d66e5f98ef11ee3eb325ff258f46
Park Junwoo <ays99111@naver.com>
2024-08-03 21:03:40 +0900

    [Feat/#16] PR 피드백 후 수정

---
+++ TurtleNeck/TurtleNeck/Domain/UseCase/NotificationManager.swift
@@ -0,0 +1,118 @@
+//
+//  notiManager.swift
+//  TurtleNeck
+//
+//  Created by 박준우 on 7/29/24.
+//
+
+import Foundation
+import UserNotifications
+
+class NotificationManager {
+    
+    private let notiCenter = UNUserNotificationCenter.current()
+    
+    // 알림 권한 상태 받아오기
+    func fetchNotiPermissionState() {
+        
+        notiCenter.getNotificationSettings { setting in
+            
+            switch setting.authorizationStatus {
+            case .notDetermined:
+                print("알림 권한 미설정 상태")
+                // TODO: 알림 권한 미설정 상태일 경우 권한 요청 자동으로 할지 결정하기
+                self.requestNotiPermission()
+            case .denied:
+                print("알림 권한 불허용 상태")
+            case .authorized:
+                print("알림 권한 허용 상태")
+            case .provisional:
+                print("알림 권한 임시 허용 상태")
+            case .ephemeral:
+                // App Clip only
+                print("알림 권한 일정시간 허용 상태")
+            default:
+                print("알림 권한 상태 가져오기 오류")
+            }
+        }
+    }
+    
+    // 알림 권한 요청하기
+    private func requestNotiPermission() {
+        
+        notiCenter.requestAuthorization(options: [.alert, .badge, .sound]) { isGranted, error in
+            if (isGranted) && (error == nil) {
+                print("알림 권한 허용")
+            }
+            else if (!isGranted) {
+                print("알림 권한 거부")
+            }
+            else{
+                print("알림 권한 오류: \(error.debugDescription)")
+            }
+        }
+    }
+    
+    // 알림 설정하기
+    func settingTimeNoti(state: NotiContentState) {
+        
+        let notiContent = getNotiContent(state: state)
+        let notiCycle = getNotiCycle(state: state)
+        let id = UUID().uuidString
+        let notiTrigger = UNTimeIntervalNotificationTrigger(timeInterval: notiCycle, repeats: false)
+        
+        let notiRequest = UNNotificationRequest(identifier: id, content: notiContent, trigger: notiTrigger)
+        notiCenter.add(notiRequest) { error in
+            if (error != nil){
+                print("알림 추가 오류: \(error.debugDescription)")
+            }
+        }
+    }
+    
+    // 설정된 알림 전체 제거하기
+    func removeTimeNoti() {
+        notiCenter.removeAllPendingNotificationRequests()
+    }
+    
+    // 알림 컨텐츠 가져오기
+    private func getNotiContent(state: NotiContentState) -> UNMutableNotificationContent{
+        
+        switch state {
+        case .worse:
+            guard let content = WorseNotiContent.contents.randomElement()?.notiContent else{
+                return UNMutableNotificationContent()
+            }
+            return content
+        case .bad:
+            guard let content = BadNotiContent.contents.randomElement()?.notiContent else{
+                return UNMutableNotificationContent()
+            }
+            return content
+        case .good:
+            guard let content = GoodNotiContent.contents.randomElement()?.notiContent else{
+                return UNMutableNotificationContent()
+            }
+            return content
+        case .normal:
+            guard let content = NormalNotiContent.contents.randomElement()?.notiContent else{
+                return UNMutableNotificationContent()
+            }
+            return content
+        }
+    }
+    
+    // 알림 주기 가져오기
+    private func getNotiCycle(state: NotiContentState) -> Double{
+        
+        switch state {
+        case .worse:
+            return 1
+        case .bad:
+            return 1
+        case .good:
+            return 1
+        case .normal:
+            return 1
+        }
+    }
+}

59a34f7419450e6d42a20823cb000db51a172c6e
HyunJaeyeon <bomul0109@gmail.com>
2024-08-06 11:53:59 +0900

    [Feat/#40] 날짜가 바뀔 때 새로운 notiStatistic 생성

--- TurtleNeck/TurtleNeck/Presentation/Main/MainView.swift
+++ TurtleNeck/TurtleNeck/Presentation/Main/MainView.swift
@@ -6,14 +6,20 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MainView: View {
     @Environment(\.appDelegate) var appDelegate: AppDelegate?
+    @Environment(\.modelContext) var modelContext
     
     @StateObject private var motionManager = HeadphoneMotionManager()
     @State var isRealTime: Bool = true
     @State var isMute: Bool = false
-
+    
+    @State private var lastCheckedDate = Date()
+    
+    @Query var statistic: [NotiStatistic]
+    
     var body: some View {
         VStack(spacing: 0){
             HStack(alignment: .center, spacing: 10){
@@ -35,6 +41,12 @@ struct MainView: View {
         .frame(width: 348,height: 232)
         .onAppear {
             motionManager.startUpdates()
+            checkDateChange()
+            
+            print(statistic.count)
+        }
+        .onChange(of: lastCheckedDate) { _, _ in
+            checkDateChange()
         }
     }
     
@@ -80,7 +92,32 @@ struct MainView: View {
             }
         }
     }
-
+    
+    private func checkDateChange() {
+        let currentDate = Date()
+        let calendar = Calendar.current
+        
+        // 마지막 체크한 날짜와 현재 날짜의 년, 월, 일이 다르면 날짜 변경 감지
+        if !calendar.isDate(lastCheckedDate, inSameDayAs: currentDate) {
+            addNotiStatistic(for: currentDate)
+        }
+        
+        lastCheckedDate = currentDate
+    }
+    
+    private func addNotiStatistic(for date: Date) {
+        // NotiStatistic의 새 항목 추가
+        let newStatistic = NotiStatistic(date: date)
+        modelContext.insert(newStatistic)
+        print("날짜 변경! statistic 추가됨")
+        
+        // statistic 배열의 길이가 7을 초과하면 첫 번째 요소 삭제
+        if statistic.count > 7 {
+            if let firstStatistic = statistic.first {
+                modelContext.delete(firstStatistic)
+            }
+        }
+    }
 }
 
 extension MainView {

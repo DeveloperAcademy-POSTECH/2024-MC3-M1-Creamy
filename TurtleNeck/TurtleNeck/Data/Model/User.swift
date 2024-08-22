31a22523faa3bbf29a1b93a504c13fbe0c4509da
Park Junwoo <ays99111@naver.com>
2024-08-23 02:12:14 +0900

    [Feat/#80] UserModel 수정

--- TurtleNeck/TurtleNeck/Data/Model/User.swift
+++ TurtleNeck/TurtleNeck/Data/Model/User.swift
@@ -19,4 +19,9 @@ struct User: Codable {
     var disturbMode : Bool = false
     var notiCycle: Double = 10 //TODO: 10초 -> 10분으로 수정
     var notificationMode: NotificationMode?
+    var isSoundOn: Bool = true // 알림소리 On/Off: 기본 값 On(true)
+    var isNotificationOn: Bool = true // 알림 On/Off: 기본 값 On(true)
+    var timeNotiCycle: Double = 15 * 60 // 시간 알림 시간: 기본 값 15분
+    var badNotiCycle: Double = 10 // 나쁜 자세 유지 시간(일반 알림): 기본 값 10초
+    var worseNotiCycle: Double = 3 * 60 // 나쁜 자세 유지 시간(캐릭터 알림): 기본 값 3분
 }

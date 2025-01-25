26c82d5b7fb1356726f4c379c3825fdd698cc885
Seo <kirin2211@naver.com>
2025-01-25 14:29:04 +0900

    [FEAT/#106] Create UserHandler

---
+++ TurtleNeck/TurtleNeck/Helper/UserHandler.swift
@@ -0,0 +1,40 @@
+//
+//  UserHandler.swift
+//  TurtleNeck
+//
+//  Created by Doran on 1/25/25.
+//
+
+import SwiftUI
+
+class UserHandler: ObservableObject {
+    @AppStorage("user") private var userData: Data = Data()
+    @Published var user: User = User(isFirst: true)
+    
+    init(){
+        loadUser()
+    }
+    
+    func loadUser() {
+        if let decodedUser = try? JSONDecoder().decode(User.self, from: userData) {
+            self.user = decodedUser
+        }
+    }
+    
+    func saveUser() {
+        if let encodedData = try? JSONEncoder().encode(user) {
+            self.userData = encodedData
+        }
+    }
+    
+    func deleteUser() {
+        UserDefaults.standard.removeObject(forKey: "user")
+        self.user = User(isFirst: true)
+        saveUser()
+    }
+    
+    func updateUser<T>(keyPath: WritableKeyPath<User, T>, value: T) {
+        user[keyPath: keyPath] = value
+        saveUser()
+    }
+}
